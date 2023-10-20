f7f55ab1cfd407ae550088769a1ea7c8973c1fde
Dilanka Silva <dilankasilva725@gmail.com>
2023-10-20 17:54:34 +0530

    Responsive Navigation Bar
    The navigation bar is adapts to different screen sizes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ServiceProviderAccount from './Pages/ServiceProviderAccount';
+import NavBar from './Component/NavBar';
 
 
 
 function App() {
   return (
     <div>
-      <ServiceProviderAccount/>
+      <NavBar/>
     </div>
   );
 }
