7588d2921c5c81b005ba6c966a1245771122d35d
Kanchana <thennakoon.kanchana@gmail.com>
2023-10-22 21:11:13 +0530

    Error Corrections

--- src/Pages/PartTimeSelect.js
+++ src/Pages/PartTimeSelect.js
@@ -2,24 +2,31 @@
 
 import React from "react";
 import '../CSS/GetJob.css';
+import NavBar from "../Component/NavBar";
+import Footer from "../Component/Footer";
 
 
 
 function GetJob() {
     return (
-        
-        <div className="flex-container">
+        <div>
+          <NavBar/>
+          <div className="flex-container">
 
-        <div className="left-side">
-        <div className="title">
-                <h1>Choose Your Job</h1>
+            <div className="left-side">
+              <br/><br/><br/><br/><br/><br/>
+              <div className="title">
+                      <h1>Choose Your Job</h1>
+              </div>
+              
+                <button>Online</button>
+                <button>Give and Take</button>
+                <button>Go To Place</button>
             </div>
-          <button>Online</button>
-          <button>Give and Take</button>
-          <button>Go To Place</button>
+        
+          </div>
+          <Footer/>
         </div>
-       
-      </div>
     );
 
 }
