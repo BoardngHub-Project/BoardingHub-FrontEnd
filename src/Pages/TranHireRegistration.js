21946f068bc17e2ca38ccf9ad8a5884fabe36c56
Dilanka Silva <dilankasilva725@gmail.com>
2023-12-10 14:30:01 +0530

    update submit button handling part
    -update submit part
    -add new css

--- src/Pages/TranHireRegistration.js
+++ src/Pages/TranHireRegistration.js
@@ -1,86 +1,87 @@
 import React, { useState } from "react";
-import driver from "../Assets/Transpottion/driver.avif";
+import Driver from "../Assets/Transpottion/driver.avif";
 import "../CSS/TranHireRegistration.css";
 import NavBar from "../Component/NavBar";
+import ImageUploader from "../Component/ImageUploader";
 
 
-function TranHireRegistration() {
-  
-  const [selectedFile, setSelectedFile] = useState(null);
+const TranHireRegistration = () => {
+  const[make,setMake] = useState("");
+  const[type,setType] = useState("");
+  const[driverName,setDriverName] = useState("");
+  const[description,setDescription] = useState("");
+  const[images,setImages] = useState([]);
+
+  const handleMakeChange = (event) => setMake(event.target.value);
+  const handleTypeChange = (event) => setType(event.target.value);
+  const handleDriverNameChange = (event) => setDriverName(event.target.value);
+  const handleDescriptionChange = (event) => setDescription(event.target.value);
+  const handleImageUpload = (newImages) => setImages([...images, ...newImages])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send data to backend
+    console.log({ make, type, driverName, images });
+  };
 
   return (
-    <div class="container">
+    <div class="flex-container">
       <NavBar/>
-      <div class="row">
-        <div class="col">
-          <img src={driver} alt=" " class="image" />
-        </div>
-        <div class="col">
-          <div class="mb-3">
+      <div class="container">
+        <img src={Driver} alt="..." />
+      </div>
+      <div class="container">
+        <h3>Create Your Ad</h3>
+        <form onSubmit={handleSubmit}>
+     
             {/* <label for="exampleFormControlInput1" class="form-label">Vehicle Type </label> */}
             <input
               type="text"
-              class="form-control"
-              id="exampleFormControlInput1"
+              id="type"
+              name="type"
+              value={type}
+              onChange={handleTypeChange}
               placeholder="Vehicle Type"
             />
-          </div>
-          <div class="mb-3">
+    
+    
             {/* <label for="exampleFormControlInput1" class="form-label">Vehicle Make</label> */}
             <input
               type="text"
-              class="form-control"
-              id="exampleFormControlInput1"
+              id="make"
+              name="make"
+              value={make}
+              onChange={handleMakeChange}
               placeholder="Vehicle Make"
             />
-          </div>
-          <div class="mb-3">
+      
             {/* <label for="exampleFormControlInput1" class="form-label">Driver Name</label> */}
             <input
               type="text"
-              class="form-control"
-              id="exampleFormControlInput1"
+              id="driverName"
+              name="driverName"
+              value={driverName}
+              onChange={handleDriverNameChange}
               placeholder="Driver Name"
             />
-          </div>
-          <div class="mb-3">
             {/* <label for="exampleFormControlTextarea1" class="form-label">Description</label> */}
             <textarea
               class="form-control"
-              id="exampleFormControlTextarea1"
+              id="description"
+              name="description"
+              value={description}
+              onChange={handleDescriptionChange}
               rows="3"
               placeholder="Give additional details"
             ></textarea>
-          </div>
-          <label for="exampleFormControlTextarea1" class="form-label">
-            Input Images Of Your Vehicle
-          </label>
+            
+       
 
-          <div className="mediaInput">
-            <input type="file" accept="image/*, video/*" />
-            {selectedFile && (
-              <div>
-                <p>Selected File: {selectedFile.name}</p>
-                {selectedFile.type.startsWith("image/") && (
-                  <img
-                    src={URL.createObjectURL(selectedFile)}
-                    alt="Selected"
-                    style={{ maxWidth: "100%", maxHeight: "200px" }}
-                  />
-                )}
-                {selectedFile.type.startsWith("video/") && (
-                  <video controls width="100%" height="200px">
-                    <source
-                      src={URL.createObjectURL(selectedFile)}
-                      type={selectedFile.type}
-                    />
-                    Your browser does not support the video tag.
-                  </video>
-                )}
-              </div>
-            )}
-          </div>
-        </div>
+   
+          <ImageUploader onUpload={handleImageUpload} />
+
+          <button type="submit">Register Vehicle</button>
+        </form>
       </div>
     </div>
   );
