6680e4b8276022c055f338cd06e3ca0c169b1fe0
Salitha Karunarathne <121235955+salithakarunarathne@users.noreply.github.com>
2024-02-11 14:52:06 +0530

    Created Job opportunities pages

--- src/Pages/CreateJobOpportunity.js
+++ src/Pages/CreateJobOpportunity.js
@@ -1,12 +1,47 @@
-import React from "react";
-
-import './CreateJobOpportunity.css';
 
 
+import '../CSS/CreateJobOpportunity.css';
+
+import React, { useState } from "react";
+import NavBar from "../Component/NavBar";
+import Submit from "../Assets/signup.avif";
+import axios from "axios";
 
 function CreateJobOpportunity() {
+  const [CompanyName, setCompanyName] = useState("");
+  const [TitleOfTheJob, setTitleOfTheJob] = useState("");
+  const [Description, setDescription] = useState("");
+  const [ContactNumber, setContactNumber] = useState("");
+  const [NumberOfOpportunity, setNumberOfOpportunity] = useState("");
+  const [Salary, setSalary] = useState("");
+  const [DateTime, setDateTime] = useState("");
+  const [PartTime, setPartTime] = useState("false");
+  const [FullTime, setFullTime] = useState("false");
+  const [Online, setOnline] = useState("false");
+  const [GiveandTake, setGiveandTake] = useState("false");
+  const [GoToPlace, setGoToPlace] = useState("false");
+
+  async function save(event) {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:8080/api/register/create-job", {
+        CompanyName: CompanyName,
+        TitleOfTheJob:TitleOfTheJob,
+        ContactNumber: ContactNumber,
+        NumberOfOpportunity: NumberOfOpportunity,
+        Description: Description,
+        Salary: Salary,
+        DateTime:DateTime
+        
+      });
+      alert("Job Oppp Created Successfully");
+    } catch (err) {
+      alert(err);
+    }
+  }
   return (
     <div class="container">
+      {/* <NavBar/> */}
       <div class="row justify-content-md-center">
         <div class="col col-lg-5">
           <p class="text-start large-text">
@@ -14,71 +49,221 @@ function CreateJobOpportunity() {
           </p>
           <div class="row">
             <div class="col">
-              <label for="lastnameController" class="form-label">
+              <label for="TitleOfTheJobController" class="form-label">
                 Company Name
               </label>
               <input
                 type="text"
                 class="form-control"
-                placeholder="Company Name"
-                aria-label="Company Name"
+                placeholder=" Company Name"
+                id="CompanyName"
+                value={CompanyName}
+                onChange={(event) => {
+                  setCompanyName(event.target.value);
+                }}
               />
             </div>
             <div class="col">
-              <label for="lastnameController" class="form-label">
-                Number Of Opportunity
+              <label for="TitleOfTheJobController" class="form-label">
+                Title Of The Job
               </label>
               <input
-                type="number"
+                type="text"
                 class="form-control"
-                placeholder="Number Of Opportunity"
-                aria-label="Number Of Opportunity"
+                placeholder="Title Of The Job"
+                id="TitleOfTheJob"
+                value={TitleOfTheJob}
+                onChange={(event) => {
+                  setTitleOfTheJob(event.target.value);
+                }}
               />
             </div>
           </div>
-          <div class="mb-3">
+          <div class="mb-3 mt-3">
             <label for="exampleFormControlInput1" class="form-label">
-              Email address
+            Description
             </label>
             <input
-              type="email"
+              type="Description"
               class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
+              id="Description"
+              placeholder="Description"
+              value={Description}
+              onChange={(event) => {
+                setDescription(event.target.value);
+              }}
+            />
+          
+          </div>
+        
+        {/* check box 1*/}
+        <p class="text-start">Service Type</p>
+          <div class="row">
+           
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  // value=""
+                  id="PartTime"
+                  value = {PartTime}
+                  onChange = {(event) => {
+                    setPartTime(event.target.value);
+                  }}
+                />
+                <label class="form-check-label" for="PartTime">
+              Part Time
+                </label>
+              
+                <input
+                  className="form-check-input1"
+                  type="checkbox"
+                  // value=""
+                  id="FullTime"
+                  value = {FullTime}
+                  onChange = {(event) => {
+                    setFullTime(event.target.value);
+                  }}
+                />
+                <label class="form-check-label" for="FullTime">
+                Full Time
+                </label>
+              </div>
+        
+          </div>
+          
+          {/* check box 2*/}
+        <p class="text-start">job Type</p>
+          <div class="row">
+           
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  // value=""
+                  id="Online"
+                  value = {Online}
+                  onChange = {(event) => {
+                    setOnline(event.target.value);
+                  }}
+                />
+                <label class="form-check-label" for="Online">
+                Online
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  // value=""
+                  id="GiveandTake"
+                  value = {GiveandTake}
+                  onChange = {(event) => {
+                    setGiveandTake(event.target.value);
+                  }}
+                />
+                <label class="form-check-label" for="GiveandTake">
+                Give and Take
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  // value=""
+                  id="GoToPlace"
+                  value = {GoToPlace}
+                  onChange = {(event) => {
+                    setGoToPlace(event.target.value);
+                  }}
+                />
+                <label class="form-check-label" for="GoToPlace">
+                Go To Place
+                </label>
+              </div>
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">
+            Contact Number
+            </label>
+            <input
+              type="Contact Number"
+              class="form-control"
+              id="ContactNumber"
+              placeholder="0XXXXXXXXX"
+              value={ContactNumber}
+              onChange={(event) => {
+                setContactNumber(event.target.value);
+              }}
             />
           
           </div>
 
           <div class="mb-3">
-            <label for="inputPassword5" class="form-label">
-              Password
+            <label for="exampleFormControlInput1" class="form-label">
+            Number Of Opportunity
             </label>
             <input
-              type="password"
+              type="Number Of Opportunity"
               class="form-control"
-              placeholder="Password"
+              id="NumberOfOpportunity"
+              placeholder="XX"
+              value={NumberOfOpportunity}
+              onChange={(event) => {
+                setNumberOfOpportunity(event.target.value);
+              }}
             />
+          
           </div>
+
           <div class="mb-3">
-            <label for="confirmPasswordController" class="form-label">
-              Confirm Password
+            <label for="exampleFormControlInput1" class="form-label">
+              Salary
             </label>
             <input
-              type="password"
+              type="Salary"
               class="form-control"
-              placeholder="Confirm Password"
+              id="Salary"
+              placeholder="XXXXXXXXXX"
+              value={Salary}
+              onChange={(event) => {
+                setSalary(event.target.value);
+              }}
             />
+          
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">
+            DateTime
+            </label>
+            <input
+              type="DateTime"
+              class="form-control"
+              id="DateTime"
+              placeholder="YYYY-MM-DD  TT.TT"
+              value={DateTime}
+              onChange={(event) => {
+                setDateTime(event.target.value);
+              }}
+            />
+          
           </div>
 
           <div class="col-12 d-flex justify-content-center">
-            <button class="btn btn-primary btn-block" type="submit">
-              Sign Up
+            <button
+              class="btn btn-primary btn-block"
+              type="submit"
+              onClick={save}
+            >
+              Submit
             </button>
           </div>
         </div>
         <div class="col-md-auto">
           <div class="text-center">
-            <img src={confirm} class="rounded" alt="..." />
+            <img src={Submit} class="rounded" alt="..." />
           </div>
         </div>
       </div>
@@ -86,78 +271,4 @@ function CreateJobOpportunity() {
   );
 }
 
-export default ServiceProviderAccount;
-
-import React, { useState } from 'react';
-import "../CSS/CreateJobOpportunity.css";
-
-function CreateJobOpportunity() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <div>
-      <button onClick={togglePopup}>Create Job Opportunity</button>
-      {isOpen && (
-        <div className="popup">
-          <div className="popup-content">
-            <h2>Create Job Opportunity</h2>
-            <form>
-              <label htmlFor="companyName">Company Name:</label>
-              <input type="text" id="companyName" name="companyName" />
-
-              <label htmlFor="contactNumber">Contact Number:</label>
-              <input type="text" id="contactNumber" name="contactNumber" />
-
-              <label htmlFor="numberOfOpportunity">Number Of Opportunity:</label>
-              <input type="text" id="numberOfOpportunity" name="numberOfOpportunity" />
-
-              <label htmlFor="salary">Salary:</label>
-              <input type="text" id="salary" name="salary" />
-
-              <label htmlFor="jobTitle">Title Of The Job:</label>
-              <input type="text" id="jobTitle" name="jobTitle" />
-
-              <label htmlFor="time">Time:</label>
-              <input type="text" id="time" name="time" />
-
-              <label htmlFor="businessType">Business Type:</label>
-              <input type="text" id="businessType" name="businessType" />
-
-              <label htmlFor="address">Address (Go To Place Type):</label>
-              <input type="text" id="address" name="address" />
-
-              <label htmlFor="online">Online:</label>
-              <select id="online" name="online">
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-
-              <label htmlFor="giveAndTake">Give And Take:</label>
-              <input type="text" id="giveAndTake" name="giveAndTake" />
-
-              <label htmlFor="cv">CV:</label>
-              <input type="text" id="cv" name="cv" />
-
-              <label htmlFor="goToPlace">Go To Place:</label>
-              <select id="goToPlace" name="goToPlace">
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-
-              <label htmlFor="description">Description:</label>
-              <textarea id="description" name="description"></textarea>
-
-              <button type="button" onClick={togglePopup}>Sign Up</button>
-            </form>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-}
-
-// export default CreateJobOpportunity;
+export default CreateJobOpportunity;
--- src/Pages/PartTimeMain.js
+++ src/Pages/PartTimeMain.js
@@ -3,19 +3,21 @@ import '../CSS/PartTimeMain.css';
 import jobImage from '../Assets/PartTimeJob/PartTimeMain.png';
 import Footer from '../Component/Footer';
 import NavBar from '../Component/NavBar';
-
+//import { Link } from "react-router-dom";
 
 function PartTimeMain() {
     return (
         <div>
-          <NavBar/>
+        <NavBar/>
           <div className="flex-container">
             <div className="left-side">
             <div className="title">
                 <h1>   Give and Get the Perfect Job</h1>
            </div>
 
-              <button>Get Job</button>
+           {/* <Link to="/get-job"> */}
+                <button>Get Job</button>
+              {/* </Link> */}
               <button>Give Job</button>
             </div>
             <div className="right-side">
